4a055586e9aea11b4debdb687f94ddd999242f1b
ghifarhaidar <ghifarh13@gmail.com>
2025-07-19 16:10:07 +0300

    fix bug

--- frontend/src/components/AlgorithmCard.jsx
+++ frontend/src/components/AlgorithmCard.jsx
@@ -4,7 +4,7 @@ export default function AlgorithmCard({ algorithm }) {
   return (
     <div className="card">
       <div className="card-body">
-        <h4 className="card-title">{algorithm.name.replace("_", " ")}</h4>
+        <h4 className="card-title">{algorithm.name.replaceAll("_", " ")}</h4>
         <p className="card-text mb-0">{algorithm.description}</p>
       </div>
     </div>
